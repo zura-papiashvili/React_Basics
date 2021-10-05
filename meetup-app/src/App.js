9b1d4059b7c92ff4305bb284f6e86d83400fa844
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-10-05 17:25:05 +0400

    meet-up app

---
+++ meetup-app/src/App.js
@@ -0,0 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage></AllMeetupsPage>
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetupPage></NewMeetupPage>
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritesPage></FavoritesPage>
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;

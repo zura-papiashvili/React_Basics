1933c23281e7950fbee635f211ef9f9808b210fe
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-10-06 16:47:57 +0400

    create a form

--- meetup-app/src/pages/NewMeetup.js
+++ meetup-app/src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New meetup page</div>;
+  return (
+    <section>
+      <h1>Add new meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetupPage;

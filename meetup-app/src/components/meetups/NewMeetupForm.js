2a14a02d8c830c38efebb0ec01e26e254a01caa3
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-10-07 15:41:48 +0400

    connect to firebase api

--- meetup-app/src/components/meetups/NewMeetupForm.js
+++ meetup-app/src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -20,7 +20,7 @@ function NewMeetupForm() {
       address: enteredAddress,
       description: enteredDescription,
     };
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
   return (
     <Card>
--- meetup-app/src/pages/NewMeetup.js
+++ meetup-app/src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-641d6-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
   return (
     <section>
       <h1>Add new meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }

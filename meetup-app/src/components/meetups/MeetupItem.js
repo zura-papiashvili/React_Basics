d33316bd3df37a5feb4e3ca3194bbbd503b1d095
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-10-07 16:48:36 +0400

    add store(useContext) for  favorites

--- meetup-app/src/components/meetups/MeetupItem.js
+++ meetup-app/src/components/meetups/MeetupItem.js
@@ -1,7 +1,25 @@
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
-
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-content";
 function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toogleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -14,7 +32,9 @@ function MeetupItem(props) {
           <p>{props.description} </p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toogleFavoriteStatusHandler}>
+            {itemIsFavorite ? "Remove" : "To Favorites"}
+          </button>
         </div>
       </Card>
     </li>
--- meetup-app/src/pages/Favorites.js
+++ meetup-app/src/pages/Favorites.js
@@ -1,5 +1,22 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-content";
+
 function FavoritesPage() {
-  return <div>Favorites page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p> You got no favorites</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
